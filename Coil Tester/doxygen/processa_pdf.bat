38cb104179f331b9dc07384d25cf0f065786c0fc
Ricardo Freitas <17169694+freric-51@users.noreply.github.com>
2019-11-03 21:17:01 -0300

    Add files via upload

---
+++ Coil Tester/doxygen/processa_pdf.bat
@@ -0,0 +1,45 @@
+cd latex
+call make.bat
+
+del dir*.pdf
+del .pdf
+
+del Makefile
+rmdir /S /Q d0
+rmdir /S /Q d1
+rmdir /S /Q d2
+rmdir /S /Q d3
+rmdir /S /Q d4
+rmdir /S /Q d5
+rmdir /S /Q d6
+rmdir /S /Q d7
+rmdir /S /Q d8
+rmdir /S /Q d9
+rmdir /S /Q da
+rmdir /S /Q db
+rmdir /S /Q dc
+rmdir /S /Q dd
+rmdir /S /Q de
+rmdir /S /Q df
+del *.tex
+del *.md5
+del *.sty
+del *.aux
+del *.idx
+del *.ilg
+del *.ind
+del *.log
+del *.toc
+del *.out
+del *.bat
+del *.eps
+
+rem apagar as figuras:
+del c*.*
+del d*.*
+del m*.*
+del t*.*
+del k*.*
+del l*.*
+
+cd ..
